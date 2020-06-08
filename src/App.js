8ec88227a6ae5764be0c7be8442fe90a41066898
asl0007 <shirazali1997@gmail.com>
2020-06-08 11:26:30 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { SumerianScene } from 'aws-amplify-react';
+import Amplify from 'aws-amplify';
+import Aws_exports from './aws-exports';
+import '@aws-amplify/ui/dist/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+Amplify.configure(Aws_exports);
+
+class App extends Component {
+  // Load scene with sceneName: "scene1" into dom element id: "sumerian-scene-dom-id"
+// async loadAndStartScene() {
+//   await Amplify.XR.loadScene("scene1", "sumerian-scene-dom-id");
+//   Amplify.XR.start("scene1");
+// }
+  render() {
+    return (
+      // <div id="sumerian-scene-dom-id"></div>
+      <div style={ { height: '100vh' } }>
+        <SumerianScene sceneName='scene1'/>
+      </div>
+    );
+  }
+};
 
 export default App;
